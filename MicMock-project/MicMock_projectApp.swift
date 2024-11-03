75f0cb741a9bafa8acd831da6d2911e519fef7e7
otinu <fitshter@gmail.com>
2024-11-03 10:23:53 +0900

    Initial Commit

---
+++ MicMock-project/MicMock_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicMock_projectApp.swift
+//  MicMock-project
+//
+//  Created by Apple on 2024/11/03.
+//
+
+import SwiftUI
+
+@main
+struct MicMock_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
